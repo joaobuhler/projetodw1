14f729a955c665b1ae244be7675507f09abe75c5
João Pedro Bühler <joaopedrobuhler26@gmail.com>
2025-11-14 11:17:51 -0300

    navbarPronta

--- projetoDw/src/homePaste/cabecalhoPaste/Cabecalho.jsx
+++ projetoDw/src/homePaste/cabecalhoPaste/Cabecalho.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import "./cabecalho.css";
+import Navbar from "../navbarPaste/Navbar";
 
 function Cabecalho() {
+
+  const [navbarAberta, setNavbarAberta] = useState(false);
+
+  function toggleNavbar() {
+    setNavbarAberta(!navbarAberta);
+  }
+
   return (
+    <>
     <div className="cabecalhoContainer">
       <div className="esquerdaCabecalho">
-        <button><i class="material-icons">menu</i></button>
+        <button onClick={toggleNavbar}><i class="material-icons" >menu</i></button>
         <h1>Quizado</h1>
       </div>
       <div className="direitaCabecalho">
@@ -12,7 +22,13 @@ function Cabecalho() {
         <button className="botaoPerfil"><i class="material-icons">person</i></button>
       </div>
     </div>
+
+    <Navbar aberta={navbarAberta} />
+    </>
   );
+  
 }
+
+
 
 export default Cabecalho;
--- projetoDw/src/homePaste/navbarPaste/Navbar.jsx
+++ projetoDw/src/homePaste/navbarPaste/Navbar.jsx
@@ -1,22 +1,11 @@
 import "./navbar.css";
 
-function Navbar() {
-  return <div className="navbarContainer"></div>;
-
-  const menuIcon = document.querySelector("#menuIcon");
-  const closeIcon = document.querySelector("#closeIcon");
-  const sidebar = document.querySelector("#sidebar");
-
-  menuIcon.addEventListener("click", showSidebar);
-  closeIcon.addEventListener("click", hideSidebar);
-
-  function showSidebar() {
-    sidebar.style.right = "0";
-  }
-
-  function hideSidebar() {
-    sidebar.style.right = "-100%";
-  }
+function Navbar({ aberta }) {
+  return (
+    <div className={`navbarContainer ${aberta ? "navbarAberta" : ""}`}>
+      
+    </div>
+  );
 }
 
 export default Navbar;
